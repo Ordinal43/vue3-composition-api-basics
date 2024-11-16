630131851e039e2a6029e0dad2dd8a50dca2b4e3
Bryan Ramaputra <bryan@stockbit.com>
2024-11-16 06:21:26 -0800

    update styling

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,62 @@
 <template>
-  <div>
-    <ol>
-      <li><router-link to="/">Basics</router-link></li>
-      <li><router-link :to="{ name: 'TemplateRefView' }">Template Ref</router-link></li>
-      <li><router-link :to="{ name: 'RefView' }">Basic reactivity with <code>ref()</code></router-link></li>
-      <li><router-link :to="{ name: 'ReactiveView' }"><code>ref()</code> vs <code>reactive()</code></router-link></li>
-      <li><router-link :to="{ name: 'ComputedView' }">Using <code>computed()</code></router-link></li>
-      <li><router-link :to="{ name: 'WatchView' }"><code>watch()</code> vs <code>watchEffect()</code></router-link></li>
-    </ol>
+  <div class="container">
+    <nav>
+      <router-link to="/">Basics</router-link>
+      <router-link :to="{ name: 'TemplateRefView' }">Template Ref</router-link>
+      <router-link :to="{ name: 'RefView' }">Basic reactivity with <code>ref()</code></router-link>
+      <router-link :to="{ name: 'ReactiveView' }"><code>ref()</code> vs <code>reactive()</code></router-link>
+      <router-link :to="{ name: 'ComputedView' }">Using <code>computed()</code></router-link>
+      <router-link :to="{ name: 'WatchView' }"><code>watch()</code> vs <code>watchEffect()</code></router-link>
+    </nav>
+    <div class="content">
+      <router-view/>
+    </div>
   </div>
-  <router-view/>
 </template>
 
 <style>
+body {
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100vh;
+}
+
+.container {
+  height: 100%;
+  display: flex;
+  align-items: stretch;
 }
 
 nav {
   padding: 30px;
+  overflow-y: scroll;
+  display: flex;
+  flex-direction: column;
+  background: #2c3e50;
+  flex: 0 0 20%;
+}
+
+.content {
+  flex: 1 1 auto;
 }
 
 nav a {
+  text-align: left;
   font-weight: bold;
-  color: #2c3e50;
+  color: #42b983;
+  padding: 10px;
+  text-decoration: none;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: white;
 }
 
 </style>
